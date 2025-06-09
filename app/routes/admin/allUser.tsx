29563042850e9c32003386a0188094305f568545
Nirmalendu Sarma <nehalsarma6@gmail.com>
2025-06-10 00:06:21 +0530

    second commit routing

---
+++ app/routes/admin/allUser.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const allUser = () => {
+  return (
+    <div>allUser</div>
+  )
+}
+
+export default allUser
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>dashboard pages</div>
+  )
+}
+
+export default dashboard
